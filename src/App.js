b7096bffbda637092459b9f8bc213e5a204c744d
igoist <493512132@qq.com>
2017-07-11 11:28:48 +0800

    dev: adjust the rule of initial & chess's style

--- src/App.js
+++ src/App.js
@@ -7,21 +7,34 @@ import Chess from './components/Chess'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.arr = this.props.arr
     this.rows = this.props.rows
     this.boxSize = this.props.boxSize
     console.log(this.props.arr)
     this.handleChess = this.handleChess.bind(this)
+    this.state = {
+      chess: Array(this.rows * this.rows).fill(null),
+      aIsNext: true,
+    }
   }
 
   handleChess(x, y) {
     let index = x * this.rows + y
-    if (this.arr[index]) {
-      this.arr[index] = 0
-    } else {
-      this.arr[index] = 1
-    }
-    console.log(this.arr[index])
+    const chess = this.state.chess.slice()
+    chess[index] = this.state.aIsNext ? 'a' : 'b';
+    this.setState({
+      chess: chess,
+      aIsNext: !this.state.aIsNext,
+    })
+  }
+
+  renderChess(i, x, y) {
+    return (
+      <Chess
+        key={i.toString()}
+        flag={this.state.chess[i]}
+        onClick={() => this.handleChess(x, y)}
+      />
+    );
   }
 
   render() {
@@ -29,27 +42,32 @@ class App extends Component {
       width: this.boxSize + 'px',
       margin: '0 auto'
     }
-
-    let rows = []
-
-    for (let x = 0; x < this.props.rows; x++) {
-      for (let y = 0; y < this.props.rows; y++) {
-        let index = x * this.rows + y
-        // let f = this.arr[index] > 0 ? true : false 
-        rows.push(<Chess id={index} flag={this.arr[index]} key={index.toString()} />)
-      }
-    }
+    // let rows = []
+    // for (let x = 0; x < this.rows; x++) {
+    //   for (let y = 0; y < this.rows; y++) {
+    //     let index = x * this.rows + y
+    //     // let f = this.arr[index] > 0 ? true : false 
+    //     // rows.push(<Chess id={index} flag={this.arr[index]} key={index.toString()} />)
+    //     // rows.push(<Chess id={index} flag={this.state.chess[index]} key={index.toString()} onClick={() => this.handleChess(x, y)} />)
+    //     rows.push(this.renderChess(index, x, y))
+    //   }
+    // }
+    let rows = this.state.chess.map((v, i) => {
+      return this.renderChess(i, parseInt(i / this.rows), i%this.rows)
+    })
 
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          {/*<h2>Welcome to React 欢迎来到 XXXの世界</h2>*/}
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div id="board" style={inlineStyle}>
+          {/*{rows}*/}
           {rows}
         </div>
       </div>
